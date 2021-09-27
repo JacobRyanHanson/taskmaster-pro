d5661e526a32e25ed1bdb4e9af921f75e8a5bcf7
JacobRyanHanson <jacobryanhanson@gmail.com>
2021-09-27 08:15:23 -0500

    Add style.css to accommodate user task sorting and dropping

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var tasks = {};
 
-var createTask = function(taskText, taskDate, taskList) {
+var createTask = function (taskText, taskDate, taskList) {
 	// Creates elements that make up a task item.
 	var taskLi = $("<li>").addClass("list-group-item");
 	var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(taskDate);
@@ -13,7 +13,7 @@ var createTask = function(taskText, taskDate, taskList) {
 	$("#list-" + taskList).append(taskLi);
 }
 
-var loadTasks = function() {
+var loadTasks = function () {
 	tasks = JSON.parse(localStorage.getItem("tasks"));
 
 	// If nothing is in localStorage, creates a new object to track all task status arrays.
@@ -27,30 +27,32 @@ var loadTasks = function() {
 	}
 
 	// Loops over object properties.
-	$.each(tasks, function(list, arr) {
+	$.each(tasks, function (list, arr) {
 		// Then loops over sub-array.
-		arr.forEach(function(task) {
+		arr.forEach(function (task) {
 			createTask(task.text, task.date, list);
 		});
 	});
 }
 
-var saveTasks = function() {
+var saveTasks = function () {
 	localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-$(".list-group").on("click", "p", function() {
+$(".list-group").on("click", "p", function () {
 	var text = $(this).text().trim();
 	var textInput = $("<textarea>").addClass("form-control").val(text);
 	$(this).replaceWith(textInput);
 	textInput.trigger("focus");
 });
 
-$(".list-group").on("blur", "textarea", function() {
+$(".list-group").on("blur", "textarea", function () {
 	var text = $(this).val().trim();
 	var status = $(this).closest(".list-group").attr("id").replace("list-", "");
 	var index = $(this).closest(".list-group-item").index();
-	tasks[status][index].text = text;
+	console.log(tasks)
+	console.log(tasks[status][index])
+	//tasks[status][index].text = text;
 
 	saveTasks();
 
@@ -58,18 +60,18 @@ $(".list-group").on("blur", "textarea", function() {
 	$(this).replaceWith(taskP);
 });
 
-$(".list-group").on("click", "span", function() {
+$(".list-group").on("click", "span", function () {
 	var date = $(this).text().trim();
 	var dateInput = $("<input>").attr("type", "text").addClass("form-control").val(date);
 	$(this).replaceWith(dateInput);
 	dateInput.trigger("focus");
 });
 
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("blur", "input[type='text']", function () {
 	var date = $(this).val().trim();
 	var status = $(this).closest(".list-group").attr("id").replace("list-", "");
 	var index = $(this).closest(".list-group-item").index();
-	tasks[status][index].date = date;
+	//tasks[status][index].date = date;
 
 	saveTasks();
 
@@ -77,20 +79,53 @@ $(".list-group").on("blur", "input[type='text']", function() {
 	$(this).replaceWith(taskSpan);
 });
 
+$(".card .list-group").sortable({
+	connectWith: $(".card .list-group"),
+	scroll: false,
+	tolerance: "pointer",
+	helper: "clone",
+
+	update: function (event) {
+		var tempArray = [];
+
+		$(this).children().each(function () {
+			var text = $(this).find("p").text().trim();
+			var date = $(this).find("span").text().trim();
+
+			tempArray.push({
+				text: text,
+				date: date
+			});
+		});
+
+		var arrayId = $(this).attr("id").replace("list-", "");
+		tasks[arrayId] = tempArray;
+		saveTasks();
+	}
+});
+
+$("#trash").droppable({
+	accept: ".card .list-group-item",
+	tolerance: "touch",
+	drop: function (event, ui) {
+		ui.draggable.remove();
+	},
+});
+
 // Modal was triggered.
-$("#task-form-modal").on("show.bs.modal", function() {
+$("#task-form-modal").on("show.bs.modal", function () {
 	// Clears values.
 	$("#modalTaskDescription, #modalDueDate").val("");
 });
 
 // Modal is fully visible.
-$("#task-form-modal").on("shown.bs.modal", function() {
+$("#task-form-modal").on("shown.bs.modal", function () {
 	// Highlights textarea.
 	$("#modalTaskDescription").trigger("focus");
 });
 
 // Saves button in modal that was clicked.
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-primary").click(function () {
 	// Gets form values.
 	var taskText = $("#modalTaskDescription").val();
 	var taskDate = $("#modalDueDate").val();
@@ -111,7 +146,7 @@ $("#task-form-modal .btn-primary").click(function() {
 });
 
 // Removes all tasks.
-$("#remove-tasks").on("click", function() {
+$("#remove-tasks").on("click", function () {
 	for (var key in tasks) {
 		tasks[key].length = 0;
 		$("#list-" + key).empty();
